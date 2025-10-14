c4d983c5a7f64e0657fe4799b55e376946c95848
ryba <ryba@MacBook-Pro.local>
2025-10-13 17:32:37 -0700

    initial commit

---
+++ clap_test.py
@@ -0,0 +1,27 @@
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load the audio file
+# By default, librosa resamples the audio to 22050 Hz and converts it to mono.
+y, sr = librosa.load('clap_test.au', sr=None)
+print(f'Audio loaded with a sampling rate of {sr} Hz')
+rms = librosa.feature.rms(y=y, frame_length=1024, hop_length=1024)[0]
+rms_normalized = rms / np.max(rms)
+
+# Set a threshold and find peaks
+threshold = 0.5 # Adjust as needed
+clap_frames = np.where(rms_normalized > threshold)[0]
+
+# Convert frame indices to time
+clap_times = librosa.frames_to_time(clap_frames, sr=sr/2) # this is tied to the frame length of the rms.
+
+print(f"Detected clap times: {clap_times}")
+
+# Optional: Visualize
+plt.figure(figsize=(12, 4))
+librosa.display.waveshow(y, sr=sr, alpha=0.5)
+plt.vlines(clap_times, -1, 1, color='r', linestyle='--', label='Clap Detections')
+plt.title('Clap Detection using RMS')
+plt.legend()
+plt.show()
---
+++ input_clap_test.py
@@ -0,0 +1,29 @@
+import librosa
+import pyaudio
+import matplotlib.pyplot as plt
+import numpy as np
+
+CHUNK = 1024*2  # Number of audio frames per buffer
+RATE = 22050  # Sample rate (samples per second)
+
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paFloat32, #paInt16 or paUInt8 for lower quality, need to convert to work with librosa
+              channels=1, # Mono
+              rate=RATE,
+              input=True,  # Set to True for input (microphone)
+              frames_per_buffer=CHUNK)
+
+print("Recording...")
+try:
+  while True:
+      data = stream.read(CHUNK)
+      audio_data = np.frombuffer(data, dtype=np.float32)
+      #rms = librosa.feature.rms(y=audio_data, frame_length=1024, hop_length=1024)
+      if max(audio_data) > 1:
+         print('clap')
+except KeyboardInterrupt:
+  print("Recording stopped.")
+
+stream.stop_stream()
+stream.close()
+p.terminate()
