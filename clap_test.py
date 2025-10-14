31b6bda589f07da49cb1cdb4444c8422d8e0a94a
ryba <ryba@MacBook-Pro.local>
2025-10-13 20:05:25 -0700

    python3.2 compatible

--- clap_test.py
+++ clap_test.py
@@ -5,7 +5,7 @@ import numpy as np
 # Load the audio file
 # By default, librosa resamples the audio to 22050 Hz and converts it to mono.
 y, sr = librosa.load('clap_test.au', sr=None)
-print(f'Audio loaded with a sampling rate of {sr} Hz')
+print('Audio loaded with a sampling rate of {} Hz'.format(sr))
 rms = librosa.feature.rms(y=y, frame_length=1024, hop_length=1024)[0]
 rms_normalized = rms / np.max(rms)
 
@@ -16,7 +16,7 @@ clap_frames = np.where(rms_normalized > threshold)[0]
 # Convert frame indices to time
 clap_times = librosa.frames_to_time(clap_frames, sr=sr/2) # this is tied to the frame length of the rms.
 
-print(f"Detected clap times: {clap_times}")
+print("Detected clap times: {}".format(clap_times))
 
 # Optional: Visualize
 plt.figure(figsize=(12, 4))
